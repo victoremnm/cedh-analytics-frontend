edd66452fb1b13078dec03a504baa93c11cda054
vem <victoremnm@gmail.com>
2026-01-23 01:21:23 -0800

    fix: force dynamic survival page for search params

--- src/app/survival/page.tsx
+++ src/app/survival/page.tsx
@@ -8,6 +8,8 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
+export const dynamic = "force-dynamic";
+
 interface SurvivalPoint {
   round_number: number;
   players_at_risk: number;
